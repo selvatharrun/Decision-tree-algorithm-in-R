f43c9bce77092dc8cddfbd8aeeb760fbb73f9ab3
selvatharrun <selvatharrun007@gmail.com>
2024-01-22 11:33:03 +0530

    nice commit

--- test1.r
+++ test1.r
@@ -1,1 +1,92 @@
-print("hello world")
+
+# Load the MASS package
+library(MASS)
+
+# Load the Boston Housing dataset
+data<-read.csv("HousingData.csv")
+
+# Load the library
+library(tidymodels)
+library(tidyr)
+
+# Prepare the dataset for ggplot2
+boston_data_long <- Boston %>%
+  pivot_longer(cols = everything(),
+               names_to = "variable",
+               values_to = "value")
+
+# Create a histogram for all numeric variables in one plot
+boston_histograms <- ggplot(boston_data_long, aes(x = value)) +
+  geom_histogram(bins = 30, color = "black", fill = "lightblue") +
+  facet_wrap(~variable, scales = "free", ncol = 4) +
+  labs(title = "Histograms of Numeric Variables in the Boston Housing Dataset",
+       x = "Value",
+       y = "Frequency") +
+  theme_minimal()
+
+# Plot the histograms
+print(boston_histograms)
+
+
+
+
+# Split the data into training and testing sets
+
+set.seed(123)
+data_split <- initial_split(Boston, prop = 0.75)
+train_data <- training(data_split)
+test_data <- testing(data_split)
+
+
+
+
+# Create a decision tree model specification
+tree_spec <- decision_tree() %>%
+  set_engine("rpart") %>%
+  set_mode("regression")
+
+# Fit the model to the training data
+tree_fit <- tree_spec %>%
+  fit(medv ~ ., data = train_data)
+
+
+
+
+# Make predictions on the testing data
+predictions <- tree_fit %>%
+  predict(test_data) %>%
+  pull(.pred)
+
+# Calculate RMSE and R-squared
+metrics <- metric_set(rmse, rsq)
+model_performance <- test_data %>%
+  mutate(predictions = predictions) %>%
+  metrics(truth = medv, estimate = predictions)
+
+print(model_performance)
+
+
+
+
+# Make predictions on new data
+new_data <- tribble(
+  ~crim, ~zn, ~indus, ~chas, ~nox, ~rm, ~age, ~dis, ~rad, ~tax, ~ptratio, ~black, ~lstat,
+  0.03237, 0, 2.18, 0, 0.458, 6.998, 45.8, 6.0622, 3, 222, 18.7, 394.63, 2.94
+)
+predictions <- predict(tree_fit, new_data)
+print(predictions)
+
+
+
+# Load the library
+library(rpart.plot)
+
+# Plot the decision tree
+rpart.plot(tree_fit$fit, type = 4, extra = 101,roundint=FALSE, under = TRUE, cex = 0.8, box.palette = "auto")
+
+# Load the necessary library
+library(vip)
+
+# Create a variable importance plot
+var_importance <- vip::vip(tree_fit, num_features = 10)
+print(var_importance)
